68fa8a3b3f67f16a36839bc327e5fd94848f0379
DaanSchar <72268586+DaanSchar@users.noreply.github.com>
2021-03-13 23:21:26 +0100

    Name changes and additions for orbit calculation

--- src/main/java/domain/MovingObject.java
+++ src/main/java/domain/MovingObject.java
@@ -1,33 +1,42 @@
-package domain;
+package main.java.domain;
 
-import interfaces.Vector3dInterface;
+import main.java.interfaces.Vector3dInterface;
 
 public class MovingObject extends SpaceObject {
-    private double velocity;
-    private Vector3dInterface newVectorState;
+    private Vector3dInterface force;
+    private Vector3dInterface acceleration;
+    private Vector3dInterface velocity;
 
     /**
      * @param mass   - the mass of the object in kilograms
-     * @param vector - the Vector Object with coordinates
+     * @param position - the Vector Object with coordinates
      */
-    public MovingObject(double mass, Vector3dInterface vector, Vector3dInterface newVectorState) {
-        super(mass, vector);
-        this.newVectorState = newVectorState;
-    }
-
-    public double getVelocity() {
-        return velocity;
-    }
-
-    public void setVelocity(double velocity) {
+    public MovingObject(double mass, Vector3dInterface position, Vector3dInterface velocity) {
+        super(mass, position);
         this.velocity = velocity;
     }
 
-    public Vector3dInterface getNewVectorState() {
-        return newVectorState;
+    public void setVelocity(Vector3dInterface velocity) {
+        this.velocity = velocity;
     }
 
-    public void setNewVectorState(Vector3dInterface newVectorState) {
-        this.newVectorState = newVectorState;
+    public Vector3dInterface getVelocity() {
+        return velocity;
+    }
+
+    public void setForce(Vector3dInterface force) {
+        this.force = force;
+    }
+
+    public Vector3dInterface getForce() {
+        return force;
+    }
+
+    public void setAcceleration(Vector3dInterface acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public Vector3dInterface getAcceleration() {
+        return acceleration;
     }
 }
