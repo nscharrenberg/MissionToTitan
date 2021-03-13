d1ce91c60d49e0d57e99283505f263cdbd39587c
DaanSchar <72268586+DaanSchar@users.noreply.github.com>
2021-03-13 23:20:49 +0100

    some namechanges in SpaceObject

--- src/main/java/domain/SpaceObject.java
+++ src/main/java/domain/SpaceObject.java
@@ -1,24 +1,30 @@
-package domain;
+package main.java.domain;
 
-import interfaces.Vector3dInterface;
+import main.java.interfaces.Vector3dInterface;
 
 public class SpaceObject {
     private double mass;
-    /**
-     * TODO: vector field did not have any assigned type, I've added it to the Vector3DInterface, however, this should be looked at.
-     */
-    private Vector3dInterface vector;
+    private Vector3dInterface position;
 
     /**
      * @param mass - the mass of the object in kilograms
-     * @param vector - the Vector Object with coordinates
+     * @param position - the Vector Object with coordinates
      */
-    public SpaceObject(double mass, Vector3dInterface vector){
+    public SpaceObject(double mass, Vector3dInterface position){
         this.mass = mass;
-        this.vector = vector;
+        this.position = position;
     }
 
     public double getMass() {
         return mass;
     }
+
+    public Vector3dInterface getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector3dInterface position) {
+        this.position = position;
+    }
 }
+
