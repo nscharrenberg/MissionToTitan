63f249148505042d495c106311b1e811e545e031
Filip <fil.rehburg@gmail.com>
2021-03-16 10:39:29 +0100

    PositionConverter added

--- src/main/java/repositories/SolarSystemRepository.java
+++ src/main/java/repositories/SolarSystemRepository.java
@@ -1,8 +1,9 @@
 package repositories;
 
-import main.java.domain.Planet;
-import main.java.domain.Vector3D;
-import main.java.repositories.interfaces.SolarSystemInterface;
+import domain.Planet;
+import domain.Vector3D;
+import repositories.interfaces.SolarSystemInterface;
+import utils.PlanetReader;
 
 import java.util.*;
 
