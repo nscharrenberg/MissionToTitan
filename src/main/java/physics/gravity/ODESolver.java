0d80d42211b2388b75f127dacfea7991929d0062
Daan <Daan.Schaer@Gmail.com>
2021-03-16 14:22:47 +0100

    implemented chaos

--- src/main/java/physics/gravity/ODESolver.java
+++ src/main/java/physics/gravity/ODESolver.java
@@ -12,11 +12,25 @@ public class ODESolver implements ODESolverInterface {
 
     @Override
     public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double tf, double h) {
-        return new StateInterface[0];
+        int size = (int)Math.round(tf/h)+1;
+        StateInterface[] stateArray = new StateInterface[size];
+
+        stateArray[0] = step(f,0, y0,h); // initial calculation of t=0
+
+        for (int i = 1; i < size; i++) {
+            //TODO: step();
+            stateArray[i] = step(f,i*h, stateArray[i-1],h);
+            State state = (State) stateArray[i];
+            state.getObject().setPosition(state.getPosition());
+            state.getObject().setVelocity(state.getVelocity());
+            System.out.println(i);
+        }
+        return stateArray;
     }
 
     @Override
     public StateInterface step(ODEFunctionInterface f, double t, StateInterface y, double h) {
-        return null;
+        // y[i+1] = y[i] + h * f.call(t[i], y[i])
+        return y.addMul(h,f.call(t,y));
     }
 }
--- src/main/java/physics/gravity/State.java
+++ src/main/java/physics/gravity/State.java
@@ -1,5 +1,7 @@
 package physics.gravity;
 
+import domain.MovingObject;
+import domain.Planet;
 import interfaces.RateInterface;
 import interfaces.StateInterface;
 import interfaces.Vector3dInterface;
@@ -8,15 +10,20 @@ public class State implements StateInterface {
 
     private Vector3dInterface position;
     private Vector3dInterface velocity;
+    private MovingObject object;
 
-    public State (Vector3dInterface position, Vector3dInterface velocity) {
+    public State (Vector3dInterface position, Vector3dInterface velocity, MovingObject object) {
         this.position = position;
         this.velocity = velocity;
+        this.object = object;
     }
 
     @Override
     public StateInterface addMul(double step, RateInterface rate) {
-        return null;
+        Rate r = (Rate) rate;
+        Rate mul = new Rate(r.getAcceleration().mul(step), r.getVelocity().mul(step));
+        State state = new State(position.add(mul.getVelocity()), velocity.add(mul.getAcceleration()),object);
+        return state;
     }
 
     public void setPosition(Vector3dInterface position) {
@@ -33,5 +40,17 @@ public class State implements StateInterface {
 
     public Vector3dInterface getVelocity() {
         return velocity;
+    }
+
+    public void setObject(MovingObject object) {
+        this.object = object;
+    }
+
+    public MovingObject getObject() {
+        return object;
+    }
+
+    public String toString() {
+        return "this is a state, cool right?";
     }
 }
