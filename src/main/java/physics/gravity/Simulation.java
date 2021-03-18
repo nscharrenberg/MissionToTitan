9a7a83a4d2f17e228f5b835aba3e11effd5b816a
Daan <Daan.Schaer@Gmail.com>
2021-03-19 00:25:57 +0100

    Simulation class added

--- src/main/java/physics/gravity/Simulation.java
+++ src/main/java/physics/gravity/Simulation.java
@@ -28,50 +28,17 @@ public class Simulation {
         timeLineArray = odes.getData(odef, totalTime, dt);
 
         State start = (State) timeLineArray[3][0];
-        double min = Double.MAX_VALUE;
-        StateInterface minState;
-        minState = start;
-        int xIndex = 0;
-        int yIndex = 0;
-        int zIndex = 0;
-        int kIndex = 0;
-        Vector3dInterface vBest = new Vector3D(0,0,0);
-        for (int x = 35000; x < 50000; x+=100) {
-            for(int y = -70000; y > -85000; y-=100) {
-                for (int z = -100; z < 100; z += 10) {
 
-                    for (int k = 45000; k < 60000; k += 100) {
-                        double newZ = z/10;
-                        Vector3dInterface v = new Vector3D(x, y, newZ);
-                        v = v.mul(k/v.norm());
-                        FactoryProvider.getSolarSystemFactory().init(v);
-                        timeLineArray = odes.getData(odef, totalTime, dt);
-                        System.out.println("x: " + x + " y: " + y + " z: " + newZ + " k: " + k );
-                        System.out.println(printMin());
-                        if (min > printMin()) {
-                            min = printMin();
-                            xIndex = x;
-                            yIndex = y;
-                            zIndex = z;
-                            kIndex = k;
-                            vBest = v;
-
-                        }
-                    }
-                }
+        for (int i = 21000; i < 25000; i+=10) {
+            for (int j = 45000; j < 60000; j+=50) {
+                State goal = (State) timeLineArray[8][i];
+                Vector3dInterface velocity = unitVecToGoal(start.getPosition(), goal.getPosition()).mul(j);
+                FactoryProvider.getSolarSystemFactory().init(velocity.add(start.getVelocity()));
+                timeLineArray = odes.getData(odef, totalTime, dt);
+                printMin();
+                System.out.println(".    i: " + i +". j: " + j);
             }
         }
-
-        System.out.println(xIndex);
-        System.out.println(yIndex);
-        System.out.println(zIndex);
-        System.out.println(kIndex);
-        System.out.println(vBest);
-
-
-
-
-
     }
 
     private static Vector3dInterface unitVecToGoal(Vector3dInterface start, Vector3dInterface goal) {
@@ -79,7 +46,7 @@ public class Simulation {
         return aim.mul(1.0/aim.norm());
     }
 
-    private static double printMin() {
+    private static void printMin() {
         double min = Double.MAX_VALUE;
 
         for (int i = 0; i < timeLineArray[0].length; i++) {
@@ -95,8 +62,7 @@ public class Simulation {
                 }
             }
         }
-        return min;
-        //System.out.println(min);
+        System.out.print(min);
     }
 
 
