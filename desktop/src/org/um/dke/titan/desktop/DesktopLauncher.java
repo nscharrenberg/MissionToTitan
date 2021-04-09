5469234945bafdc9c6e73755f89831547d6272da
Noah Scharrenberg <nscharrenberg@hotmail.com>
2021-04-09 13:18:46 +0200

    setup libgdx

---
+++ desktop/src/org/um/dke/titan/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.um.dke.titan.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.um.dke.titan.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
