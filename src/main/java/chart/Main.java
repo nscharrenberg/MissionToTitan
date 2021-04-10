10de7e59b71545b49d137e39ca5789519ef38166
Daan <Daan.Schaer@Gmail.com>
2021-04-10 19:06:22 +0200

    Added Runge-Kutta Method to the ProbeSimulator

--- src/main/java/chart/Main.java
+++ src/main/java/chart/Main.java
@@ -2,11 +2,13 @@ package chart;
 
 import factory.FactoryProvider;
 import interfaces.StateInterface;
+import interfaces.Vector3dInterface;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import physics.gravity.ode.ProbeSimulator;
 import physics.gravity.ode.State;
 
 public class Main extends Application {
@@ -27,13 +29,16 @@ public class Main extends Application {
     public static void run() {
         System.out.println("GRAPH:");
 
-        double dt = 100;
+        double dt = 10;
         double year = 1;
 
         System.out.println("Starting Runge-Kutta");
 
         FactoryProvider.getSolarSystemFactory().computeTimeLineArrayRunge(daySec*365.24*year,dt);
         StateInterface[][] timeLineArrayRunge = FactoryProvider.getSolarSystemFactory().getTimeLineArray(daySec*365.24*year, dt);
+
+        ProbeSimulator simulator = new ProbeSimulator();
+        Vector3dInterface[] probeArray = simulator.trajectory(((State)timeLineArrayRunge[6][0]).getPosition(), ((State)timeLineArrayRunge[6][0]).getVelocity(), daySec*365.24*year, dt);
 
         System.out.println("- Completed computing states");
 
@@ -43,9 +48,8 @@ public class Main extends Application {
         for (int t = (int)Math.round(startTime/dt)+1; t < (int)Math.round(endTime/dt)+1; t+=100) {
 
             State probe = (State)timeLineArrayRunge[6][t];
-            State titan = (State)timeLineArrayRunge[5][t];
-            ChartLoader.addDataA(t,   probe.getPosition().dist(titan.getPosition()));
 
+            ChartLoader.addDataA(t,   probe.getPosition().dist(probeArray[t]));
         }
 
         System.out.println("- Completed inserting states into graph");
