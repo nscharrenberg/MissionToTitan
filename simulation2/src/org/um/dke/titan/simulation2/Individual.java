0608ae3780f1d3a92f66fd347e2bc050d5ac6390
Noah Scharrenberg <nscharrenberg@hotmail.com>
2021-05-11 12:29:41 +0200

    push some ga stuff

--- simulation2/src/org/um/dke/titan/simulation2/Individual.java
+++ simulation2/src/org/um/dke/titan/simulation2/Individual.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class Individual {
+    private static double MAX_THRUST = 3e7;
+
     private Vector3dInterface startPosition;
     private double velocity;
     private HashMap<String, Double> engineForce;
@@ -15,58 +17,33 @@ public class Individual {
     private double distanceFromEarth;
 
     public void mutate() {
-        // TODO: Mutation Logic here
+        Random r = new Random();
+        int rand = r.nextInt(100);
+
+        // Mutation Threshold
+        if (rand > 80) {
+            if (rand < 85) {
+                startPosition.setX(startPosition.getX() * r.nextDouble());
+            } else if (rand < 90) {
+                startPosition.setY(startPosition.getY() * r.nextDouble());
+            } else if (rand < 95) {
+                startPosition.setZ(startPosition.getZ() * r.nextDouble());
+            } else {
+                velocity = velocity * r.nextInt(20000);
+            }
+        }
     }
 
     public void determineFitness() {
-        // TODO: Fitness Determination logic here
-    }
-
-    public Vector3dInterface getStartPosition() {
-        return startPosition;
-    }
-
-    public void setStartPosition(Vector3dInterface startPosition) {
-        this.startPosition = startPosition;
-    }
-
-    public double getVelocity() {
-        return velocity;
-    }
-
-    public void setVelocity(double velocity) {
-        this.velocity = velocity;
-    }
-
-    public HashMap<String, Double> getEngineForce() {
-        return engineForce;
-    }
-
-    public void setEngineForce(HashMap<String, Double> engineForce) {
-        this.engineForce = engineForce;
+        fitness = Simulation.run(startPosition, velocity, engineForce);
     }
 
     public double getFitness() {
-        return fitness;
+        return this.fitness;
     }
 
-    public void setFitness(double fitness) {
-        this.fitness = fitness;
-    }
-
-    public double getDistanceFromTitan() {
-        return distanceFromTitan;
-    }
-
-    public void setDistanceFromTitan(double distanceFromTitan) {
-        this.distanceFromTitan = distanceFromTitan;
-    }
-
-    public double getDistanceFromEarth() {
-        return distanceFromEarth;
-    }
-
-    public void setDistanceFromEarth(double distanceFromEarth) {
-        this.distanceFromEarth = distanceFromEarth;
+    @Override
+    public String toString() {
+        return String.valueOf(getFitness());
     }
 }
--- simulation2/src/org/um/dke/titan/simulation2/Simulation.java
+++ simulation2/src/org/um/dke/titan/simulation2/Simulation.java
@@ -1,11 +1,16 @@
 package org.um.dke.titan.simulation2;
 
+import org.um.dke.titan.domain.SpaceObjectEnum;
 import org.um.dke.titan.interfaces.StateInterface;
 import org.um.dke.titan.interfaces.Vector3dInterface;
+import org.um.dke.titan.physics.ode.State;
+
+import java.util.HashMap;
 
 public class Simulation {
     private static StateInterface[][] timelineArray;
     private static Vector3dInterface[] probePositions;
+    private static SimulationResults results;
 
     private static double daySec = 60*24*60;
     private static double dt = 20;
@@ -14,5 +19,45 @@ public class Simulation {
     private static double titanRadius = 2575.5e3;
     private static double earthRadius = 6371e3;
 
-    public static double run(Vector3dInterface unit, double velocity, double)
+    public static SimulationResults run(Vector3dInterface unit, int velocity, HashMap<String, Double> engineForce) {
+        simulate(unit, velocity, engineForce);
+
+        return results;
+    }
+
+    public static SimulationResults getMinDistanceToTitan() {
+        double min = Double.MAX_VALUE;
+        double time = Double.MAX_VALUE;
+
+        for (int i = 0; i < timelineArray[0].length; i++) {
+            State titan = (State) timelineArray[SpaceObjectEnum.TITAN.getId()][i];
+            Vector3dInterface probe = probePositions[i];
+
+            double dist = probe.dist(titan.getPosition()) - titanRadius;
+
+            if (min > dist && dist > 0) {
+                results.setMinDistanceToTitan(dist);
+                results.setMinTimeToTitan(i);
+            }
+        }
+
+        return results;
+    }
+
+    public static SimulationResults getMinDistanceToEarth() {
+        double min = Double.MAX_VALUE;
+        for (int i = results.getMinTimeToTitan(); i < timelineArray[0].length; i++) {
+            State earth = (State) timelineArray[SpaceObjectEnum.EARTH.getId()][i];
+            Vector3dInterface probe = probePositions[i];
+
+            double dist = probe.dist(earth.getPosition()) - earthRadius;
+
+            if (min > dist && dist > 0) {
+                results.setMinDistanceToEarth(dist);
+                results.setMinTimeToEarth(i);
+            }
+        }
+
+        return results;
+    }
 }
